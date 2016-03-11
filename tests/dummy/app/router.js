ba82e4fc39a769c626ab8b6da8be20d592838648
White, Eric <ewhite@ciena.com>
2016-03-11 11:11:50 -0500

    Change dummy app to use custom routing, so it's compatible with frost-guide

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,27 +1,15 @@
 import Ember from 'ember'
 import config from './config/environment'
+import addRoute from 'frost-guide-custom-routing/utils/addRoute'
 
 var Router = Ember.Router.extend({
   location: config.locationType
 })
 
 Router.map(function () {
-  this.route('demo', { path: '/' }, function () {
-    this.modal('frost-about-dialog', {
-      withParams: 'isAboutVisible',
-      dialogClass: 'frost-about-dialog',
-      otherParams: [
-        { dialogType: 'type' },
-        { dialogTitle: 'title' },
-        { dialogMessage: 'message' },
-        { dialogServerMessage: 'serverMessage' },
-        { dialogCopyrightText: 'copyrightText' },
-        { dialogLicenseText: 'licenseText' }
-      ],
-      actions: {
-        confirm: 'dialogConfirmed'
-      }
-    })
+    let routerConfig = config.APP.routingConfig
+  routerConfig.forEach((item) => {
+    addRoute.call(this, item)
   })
 })
 
