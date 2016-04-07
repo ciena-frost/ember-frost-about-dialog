ed766ee4f539e7b17e22a9549f676ca6a05b7e91
Roxanne Panthaky <rpanthak@ciena.com>
2016-04-07 13:59:06 -0400

    consume frost-core. use new import convention.

--- tests/dummy/app/pods/demo/controller.js
+++ tests/dummy/app/pods/demo/controller.js
@@ -1,6 +1,8 @@
 import Ember from 'ember'
 
-export default Ember.Controller.extend({
+const {Controller} = Ember
+
+export default Controller.extend({
   isAboutVisible: false,
   actions: {
     toggleAboutDialog (type) {
