d30139b8d3efd7fc4705346c30147a1b3518a7ef
Adam Meadows <adam.meadows@gmail.com>
2016-07-07 06:47:09 -0600

    Update to work with new frost-icons w/ icon packs

--- addon/components/frost-about-dialog.js
+++ addon/components/frost-about-dialog.js
@@ -1,18 +1,62 @@
+/**
+ * A component for rendering a frosty about dialog
+ */
+
 import Ember from 'ember'
+import PropTypeMixin, {PropTypes} from 'ember-prop-types'
+
 import layout from '../templates/components/frost-about-dialog'
 
 const {Component, inject} = Ember
 
-export default Component.extend({
+export default Component.extend(PropTypeMixin, {
+  // ==========================================================================
+  // Dependencies
+  // ==========================================================================
+
   remodal: inject.service(),
 
-  layout,
-  copyrightText: 'Copyright &copy; 2016 Ciena corporation, all rights reserved.',
-  licenseText: '',
+  // ==========================================================================
+  // Properties
+  // ==========================================================================
 
-  didRender () {
-    Ember.$('.ember-remodal.window').addClass('frost-about-dialog')
+  layout,
+
+  propTypes: {
+    copyrightText: PropTypes.string,
+    iconPack: PropTypes.string.isRequired,
+    licenseText: PropTypes.string,
+    logoIcon: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+    modalName: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    serverMessage: PropTypes.string.isRequired,
+    stripIcon: PropTypes.string.isRequired
   },
+
+  getDefaultProps () {
+    return {
+      copyrightText: '',
+      iconPack: 'frost',
+      licenseText: ''
+    }
+  },
+
+  // ==========================================================================
+  // Computed Properties
+  // ==========================================================================
+
+  // ==========================================================================
+  // Functions
+  // ==========================================================================
+
+  // ==========================================================================
+  // Events
+  // ==========================================================================
+
+  // ==========================================================================
+  // Actions
+  // ==========================================================================
 
   actions: {
     close () {
